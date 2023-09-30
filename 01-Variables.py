deb9e7fab0c8eec11dee2ce4ceba1d4bf77131da
Prathamesh Andhare <92220219+PrathameshAndhare@users.noreply.github.com>
2023-09-30 19:36:05 +0530

    Create 01-Variables.py
    Variable declaration and data types

---
+++ 01-Variables.py
@@ -0,0 +1,91 @@
+# Variables
+# variables are containers
+# they hold the value/data
+
+# this is new line , ignore
+
+# eg : 
+x = 100
+one = 111
+name = "Rakesh"
+one_1 = "Five"
+
+
+# allowed variables in Python
+
+i_am = 100
+iam_ = 100
+_i_am = 100
+_iam_ = 100
+
+# not allowed variables in Python
+'''
+@123 = 100
+#123 = 100
+$123$123 = 100
+. , = + - / @ * & } { ] .....
+'''
+
+# allowed but not recommended
+# dunder  = double underscore
+# they are reserved for future use
+
+__iam__ = 100
+__ = 100
+
+# place holder
+# mostly used in unpacking a tuple
+
+
+_  = 100
+print(_)
+_ = 500
+print(_)
+
+data1=20
+print(data1)
+
+_ =40
+
+print(_)
+
+# type(variable name / data) -- > shows the type of the data 
+
+q=20
+
+print(q)
+print(q)
+print(q)
+
+print(type(q))
+
+print(type(40))
+
+
+var1=False
+
+print(type(var1))
+
+r=type(40)
+print(r)
+
+
+# string 
+
+# " "
+
+string1="sunday is good ."
+
+print(string1)
+
+
+print('------------')
+
+
+string2="rainbow"
+
+print(string2)
+
+string2="apple"
+
+print(string2)
