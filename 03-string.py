c1a07ebbcdb377207d49fca37e97eff232d7b538
Prathamesh Andhare <92220219+PrathameshAndhare@users.noreply.github.com>
2023-10-06 19:37:31 +0530

    Create 03-string.py

---
+++ 03-string.py
@@ -0,0 +1,72 @@
+# string 
+
+# q="rainbow"
+# print(q)
+
+# print("rainbow")
+
+# print('What is your name')
+
+# name=input("What is your name ?")
+
+# print(name)
+
+# # name="Sanchit"
+
+# name=input("Please enter another name ? ")
+
+
+
+name="Rohit"
+print(2,3,4,55)
+print(name)
+
+print(1+34)
+print("1+34")
+
+
+# type(data)
+
+print(type(29))
+print(type(29.456))
+print(type("rainbow"))
+print(type("1+34"))
+
+
+name="rohit"
+age=20
+area="pune"
+
+# print(name,age,area)
+# print("My name is ",name,"My age is ",age,"I live in ",area)
+
+# format()
+
+# var="My name is {} , my age is {} and I live in {}".format(name,age,area)
+
+var="My name is {0} , my age is {1} and I live in {2}".format(name,age,area)
+
+var.format(name,age,area)
+print(var)
+
+
+# indexing --->
+
+#   0123456
+st="rainbow"
+
+print(st)
+print(st[3])
+print(st[0])
+print(st[5])
+
+
+
+
+
+
+
+
+
+
+
